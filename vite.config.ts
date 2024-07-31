8ebfce88a7011c184f018b942715a2f8cdfeeb1b
D4ST1N <d4st1nh0uk@gmail.com>
2024-07-31 17:14:47 +0300

    init: add aliases & base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
+  base: "/budget-x/",
 });
