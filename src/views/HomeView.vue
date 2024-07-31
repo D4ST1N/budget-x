fd586b7c116dd782f8ae7d4a94279f312654de33
D4ST1N <d4st1nh0uk@gmail.com>
2024-08-01 02:28:10 +0300

    init: Checking server connection

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log(import.meta.env);
+console.log(import.meta.env.VITE_SERVER_URL);
+fetch(import.meta.env.VITE_SERVER_URL);
+</script>
 
 <template>
   <v-btn>Add Expense</v-btn>
